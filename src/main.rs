8ff357d4f72f5574113c3fe5421d6a8fd5de641e
akatsuki-py <bluejapan73+dev@gmail.com>
2020-05-25 18:46:29 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    std::process::exit(run())
+}
+
+fn run() -> i32 {
+    let result = getTarget();
+    match result {
+        Err(e) => {
+            eprintln!("{}", e);
+            return 1;
+        },
+        Ok(target) => {
+            println!("{}", target);
+            return 0;
+        },
+    }
+}
+
+fn getTarget() -> Result<String, String> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        return Err("Parse error: rust-elf-parser <ELF>".to_string());
+    }
+
+    let target = &args[1];
+    return Ok(target.to_string());
 }
